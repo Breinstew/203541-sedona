c64568a5b4db0f31cf69263feb163992d5fe349d
Breinstew <snowway@list.ru>
2017-05-12 22:54:36 +0300

    Скрыл форму по умолчанию при включенном js.

--- js/form.js
+++ js/form.js
@@ -1,5 +1,7 @@
 var form = document.querySelector(".search-form");
 var btn = document.querySelector(".btn-search");
+
+form.classList.toggle("modal-content");
 btn.addEventListener("click",function(evt){
   evt.preventDefault();
   console.log("Клик по кнопке");
